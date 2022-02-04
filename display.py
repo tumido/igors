5acbee3696ad7786ab9b6e69769bffbafac9d42a
Tomas Coufal <coufal.tom@gmail.com>
2022-02-04 19:25:16 +0100

    feat: Upgrade partial renders on display
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 # type: ignore
 import os
+from time import sleep
 from PIL import Image, ImageDraw, ImageFont
 from dotenv import load_dotenv
 from collections import deque
@@ -35,8 +36,6 @@ THERMOMETER = [
     utf(b"\\uf2c8"),
     utf(b"\\uf2c7"),
 ]
-SENSOR_1 = 22.1
-SENSOR_2 = (22.3, 45.4)
 
 
 class Display:
@@ -67,172 +66,74 @@ def draw():
     DISPLAY.displayPartial(DISPLAY.getbuffer(IMAGE))
 
 
-# DRAW.rectangle(((DISPLAY.width - w)//2, h*line, ((DISPLAY.width - w)//2)+w, h*(line+1)))
-
-# for idx, icon in enumerate(THERMOMETER):
-#     DRAW.text((30 * idx, 0), icon, font=FA_SOLID)  # type: ignore
-# DRAW.rounded_rectangle((5, 5, 50, 50), width=4, radius=10)
-# DRAW.rounded_rectangle((5, 55, 50, 100), width=4, radius=10)
-
-DRAW.text(
-    (12, 0),
-    f"{SENSOR_2[0]:0.1f}",
-    font=BITTER_40,
-)
-DRAW.text((90, 26), "°C", font=BITTER_20)
-DRAW.line((3, 60, DISPLAY.width - 3, 60))
-for i in range(38):
-    DRAW.line((2 + (i * 3), 65, 7 + (i * 3), 60))
+def get_sensor_data(path, cast=next, val=lambda x: float(x)):
+    try:
+        with open(path) as f:
+            return cast(map(val, list(f.readlines())))
+    except:
+        return cast(map(val, [0]))
 
 
-DRAW.text(
-    (12, 50),
-    f"{SENSOR_1:0.1f}",
-    font=BITTER_40,
-)
-DRAW.text((90, 76), "°C", font=BITTER_20)
-
-
-DRAW.text(
-    (35, 105),
-    f"{SENSOR_2[1]:0.0f}",
-    font=BITTER_40,
-)
+DRAW.text((90, 16), "°C", font=BITTER_20)
+DRAW.line((3, 50, DISPLAY.width - 3, 50))
+for i in range(37):
+    DRAW.line((-10 + (i * 4), 60, 0 + (i * 4), 50))
+DRAW.line((0, 0, 0, 100), fill=1)
+DRAW.line((121, 0, 121, 110), fill=1)
+DRAW.rounded_rectangle((1, 1, 120, 110), radius=8)
+DRAW.text((90, 74), "°C", font=BITTER_20)
 DRAW.text((82, 128), "%", font=BITTER_20)
-
-# DRAW.line(((DISPLAY.width // 2), 0, (DISPLAY.width // 2), DISPLAY.height))
-# DRAW.line((0, (DISPLAY.height // 2), DISPLAY.width, (DISPLAY.height // 2)))
-
 DRAW.text((10, 180), "Topení", font=NOTO)
 
-DRAW.rounded_rectangle((27, 200, 57, 214), radius=7, fill=0)
-DRAW.text((35, 200), "On", fill=1, font=NOTO)
-DRAW.rounded_rectangle((65, 200, 95, 214), radius=7)
-DRAW.text((72, 200), "Off", font=NOTO)
-
-# DRAW.rounded_rectangle((1,1,120,110), radius=8)
-
-a = deque(
-    [
-        20.1,
-        20.3,
-        20.6,
-        21.0,
-        22.1,
-        23.3,
-        23.6,
-        23.0,
-        23.1,
-        23.3,
-        23.6,
-        23.0,
-        23.1,
-        23.3,
-        24.6,
-        24.0,
-        24.1,
-        25.3,
-        25.6,
-        25.0,
-        24.1,
-        24.3,
-        24.6,
-        24.0,
-        24.1,
-        24.3,
-        24.6,
-        24.0,
-        24.1,
-        24.3,
-        24.6,
-        24.0,
-        24.1,
-        24.3,
-        23.6,
-        23.0,
-        23.1,
-        23.3,
-        23.6,
-        23.0,
-        23.1,
-        23.3,
-        23.6,
-        23.0,
-        23.1,
-        23.3,
-        24.6,
-        24.0,
-        24.1,
-        24.3,
-        24.6,
-        24.0,
-        24.1,
-        24.3,
-        24.6,
-        24.0,
-        24.1,
-        24.3,
-        24.6,
-        24.0,
-        24.1,
-        25.3,
-        25.6,
-        25.0,
-        25.1,
-        25.3,
-        25.6,
-        25.0,
-        25.1,
-        25.3,
-        25.6,
-        25.0,
-        25.1,
-        25.3,
-        25.6,
-        26.0,
-        26.1,
-        26.3,
-        26.6,
-        26.0,
-        26.1,
-        26.3,
-        26.6,
-        26.0,
-        26.1,
-        26.3,
-        26.6,
-        26.0,
-        26.1,
-        26.3,
-        26.6,
-        26.0,
-        27.1,
-        27.3,
-        27.6,
-        27.0,
-        27.1,
-        27.3,
-        27.6,
-        27.0,
-        27.3,
-        27.6,
-        27.0,
-        27.1,
-        27.3,
-        27.6,
-        27.0,
-        27.5,
-        27.5,
-        27.2,
-    ],
-    maxlen=100,
-)
-
-
-BASE = 250
-
-for idx, i in enumerate(a):
-    DRAW.point((idx + 10, BASE - round((i - 10))))
-
-
 draw()
+
+sensor_1_temp, sensor_2_temp, sensor_2_humidity, heat = (0, 0, 0, False)
+while True:
+    sensor_1_temp_new = get_sensor_data("/tmp/sensor_1/temp")
+    sensor_2_temp_new = get_sensor_data("/tmp/sensor_2/temp")
+    sensor_2_humidity_new = get_sensor_data("/tmp/sensor_2/hum")
+    history = get_sensor_data("/tmp/sensor_2/hist", lambda m: deque(m, maxlen=100))
+    heat_new = get_sensor_data("/tmp/sensor_3/heat", val=lambda m: m == "true\n")
+
+    if sensor_2_temp != sensor_2_temp_new:
+        DRAW.rectangle((12, 5, 88, 48), fill=1)
+        sensor_2_temp = sensor_2_temp_new
+        DRAW.text(
+            (12, -10),
+            f"{sensor_2_temp:0.1f}",
+            font=BITTER_40,
+        )
+
+    if sensor_1_temp != sensor_1_temp_new:
+        DRAW.rectangle((12, 65, 88, 108), fill=1)
+        sensor_1_temp = sensor_1_temp_new
+        DRAW.text(
+            (12, 48),
+            f"{sensor_1_temp:0.1f}",
+            font=BITTER_40,
+        )
+
+    if sensor_2_humidity != sensor_2_humidity_new:
+        sensor_2_humidity = sensor_2_humidity_new
+        DRAW.rectangle((35, 125, 80, 155), fill=1)
+        DRAW.text(
+            (35, 105),
+            f"{sensor_2_humidity:0.0f}",
+            font=BITTER_40,
+        )
+
+    if heat != heat_new:
+        heat = heat_new
+        DRAW.rounded_rectangle((27, 200, 57, 214), radius=7, outline=0, fill=not heat)
+        DRAW.text((35, 200), "On", font=NOTO, fill=heat)
+        DRAW.rounded_rectangle((65, 200, 95, 214), radius=7, outline=0, fill=heat)
+        DRAW.text((72, 200), "Off", font=NOTO, fill=not heat)
+
+    for idx, i in enumerate(history):
+        DRAW.point((idx + 10, 250 - round((i - 10))))
+
+    print(
+        f"above={sensor_2_temp:0.1f} °C, below={sensor_1_temp:0.1f} °C, humidity={sensor_2_humidity:0.0f} %, heat={heat}, history={len(history)}"
+    )
+    draw()
+    sleep(60)
